44c6038ad4ef6d1c48cda8892be1d9e60872af38
Neha Sengupta <neha.sengupta@inceptioniai.org>
2019-02-12 12:46:17 +0400

    removing unnecessary files

--- samplingTime.sh
+++
@@ -1,6 +0,0 @@
-./BSTtesting 10000000 100 ../SimulatedDatasets/10M/Uniform10K_100 3 284638 0 6 0.5
-./BSTtesting 10000000 100 ../SimulatedDatasets/10M/Uniform10K_100 3 328072 0 6 0.6
-./BSTtesting 10000000 100 ../SimulatedDatasets/10M/Uniform10K_100 3 382578 0 6 0.7
-./BSTtesting 10000000 100 ../SimulatedDatasets/10M/Uniform10K_100 3 460900 0 6 0.8
-./BSTtesting 10000000 100 ../SimulatedDatasets/10M/Uniform10K_100 3 608695 0 5 0.9
-./BSTtesting 10000000 100 ../SimulatedDatasets/10M/Uniform10K_100 3 1372303 0 5 1.0
